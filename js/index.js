21fcf4f4dc700ea524786bfad5ca6123bf3675f8
yaocs <yao.cs@qq.com>
2016-07-25 21:14:02 +0800

    change 0725-03

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(function(){
     $('#content').fullpage({
-      //Navigation
         anchors:['firstPage', 'secondPage', '3rdPage'],
         navigation: true,
         navigationTooltips: ['个人简介', '工作经历', '相关作品'],
